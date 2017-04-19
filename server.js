b5bf9edb66492b8b8a69cdffd30942b929ba224b
Dakota Bryant <bryant.dakota@gmail.com>
2017-04-19 13:57:13 -0500

    added the harrison recipe

--- server.js
+++ server.js
@@ -63,9 +63,8 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
     }
   }
   if (req.params.id !== req.body.id) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id `
-      `(${req.body.id}) must match`);
+    const message = `Request path id ${req.params.id} and request body id
+      ${req.body.id} must match`;
     console.error(message);
     return res.status(400).send(message);
   }
@@ -77,6 +76,29 @@ app.put('/shopping-list/:id', jsonParser, (req, res) => {
   });
   res.status(204).json(updatedItem);
 });
+app.put('/recipes/:id', jsonParser, (req,res) => {
+  const requiredFields = ['name','ingredients', 'id'];
+  for (let i=0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = `Your ${req.body.id} does not match ${req.params.id}`
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  const updatedItem = Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
+  res.status(204).json(updatedItem);
+
+})
 
 // when DELETE request comes in with an id in path,
 // try to delete that item from ShoppingList.
